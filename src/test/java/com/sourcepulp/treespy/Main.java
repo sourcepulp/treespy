828d52988507eca1889ff0532b9b128e067f8117
Will Faithfull <will@sourcepulp.com>
2015-10-26 13:00:27 +0000

    Switched to slf4j logging

--- src/test/java/com/sourcepulp/treespy/Main.java
+++ src/test/java/com/sourcepulp/treespy/Main.java
@@ -3,7 +3,12 @@ package com.sourcepulp.treespy;
 import java.io.File;
 import java.io.IOException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
+	
+	final static Logger log = LoggerFactory.getLogger(Main.class);
 
 	public static void main(String[] args) throws IOException {
 		
@@ -14,7 +19,7 @@ public class Main {
 		File home = new File(directory);
 		
 		spy.watch(home, (changedFile, eventType) -> {
-			System.out.println(String.format("File %s changed", changedFile));
+			log.info(String.format("File %s changed, mode %s", changedFile, eventType.toString()));
 		});
 		
 		System.in.read();
