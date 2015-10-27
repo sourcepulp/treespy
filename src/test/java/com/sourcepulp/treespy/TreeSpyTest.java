936fcb45c0e8710c56940e9bc140c782cd24a761
Will Faithfull <will@sourcepulp.com>
2015-10-27 16:57:03 +0000

    Overhauled testing, simplified down to create/mod/delete in classpath. TODO: Find a way to mock out the Java API.

--- src/test/java/com/sourcepulp/treespy/TreeSpyTest.java
+++ src/test/java/com/sourcepulp/treespy/TreeSpyTest.java
@@ -1,45 +1,112 @@
 package com.sourcepulp.treespy;
-import static org.junit.Assert.assertTrue;
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.WatchService;
-import java.util.concurrent.Executor;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
+import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mockito;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import com.sourcepulp.treespy.jse7.TreeSpyJSE7StdLib;
+public class TreeSpyTest {
+	private final static Logger log = LoggerFactory.getLogger(TreeSpyTest.class);
 
-/**
- * Unit test for simple App.
- */
-public class TreeSpyTest 
-{
-	TreeSpyJSE7StdLib spy;
-	String dirName = System.getProperty("user.dir");
-	
+	final static int WAIT_TIME_MILLIS = 500;
+
+	private final CountDownLatch countDownLatch = new CountDownLatch(1);
+	private File modifyFile;
+
 	@Before
 	public void setup() throws IOException {
-		WatchService watcher = Mockito.mock(WatchService.class);
-		Executor executor = Mockito.mock(Executor.class);
-		spy = new TreeSpyJSE7StdLib(executor, watcher);
+		modifyFile = new File("testfile.txt");
+		modifyFile.createNewFile();
+	}
+
+	@After
+	public void teardown() {
+		if(modifyFile.exists())
+			modifyFile.delete();
+	}
+
+	@Test
+	public void testModify() throws IOException, InterruptedException {
+		File directory = modifyFile.getAbsoluteFile().getParentFile();
+
+		TreeSpy spy = SpyFactory.getSpy();
+
+		spy.watch(directory, (f, t) -> {
+			if (t == Events.MODIFY) {
+				log.info("Modification detected");
+				Assert.assertEquals(modifyFile.getAbsolutePath(), f.toString());
+			}
+		});
+
+		try {
+			write(modifyFile.toPath(), "hello");
+			await();
+		} finally {
+			spy.stop();
+		}
+	}
+
+	@Test
+	public void testCreate() throws IOException, InterruptedException {
+
+		File resourcesDir = new File(System.getProperty("user.dir"));
+		TreeSpy spy = SpyFactory.getSpy();
+
+		final String newFileName = "create.txt";
+		File newFile = new File(newFileName);
+
+		spy.watch(resourcesDir, (f, t) -> {
+			if (t == Events.CREATE) {
+				log.info("Creation detected");
+				Assert.assertEquals(newFileName, f.getFileName().toString());
+			}
+		});
+		try {
+			newFile.createNewFile();
+			await();
+		} finally {
+			newFile.delete();
+			spy.stop();
+		}
 	}
 	
-    @Test
-    public void testApp() throws IOException
-    {	
-    	File directory = new File(dirName);
-    	
-    	spy.watch(directory, (f,t) -> {
-    		if(t == Events.CREATE) {
-    			System.out.println(String.format("Created file %s", f.toString()));   			
-    		}
-    	});
-    	
-    	// TODO: Work out some way to test concurrently :s
-    	
-    	assertTrue( true );
-    }
+	@Test
+	public void testDelete() throws IOException, InterruptedException {
+		TreeSpy spy = SpyFactory.getSpy();
+		
+		spy.watch(modifyFile.getAbsoluteFile().getParentFile(), (f, t) -> {
+			if (t == Events.DELETE) {
+				log.info("Deletion detected");
+				Assert.assertEquals(modifyFile.getName(), f.getFileName().toString());
+			}
+		});
+		try {
+			modifyFile.delete();
+			await();
+		} finally {
+			spy.stop();
+		}
+	}
+
+	private void write(Path file, String s) throws IOException {
+		try (BufferedWriter bw = Files.newBufferedWriter(file, Charset.defaultCharset())) {
+			bw.write(s);
+		}
+	}
+
+	private void await() throws InterruptedException {
+		countDownLatch.await(WAIT_TIME_MILLIS, TimeUnit.MILLISECONDS);
+	}
+
 }
